70409513cb74d812a74e016f927f9770d59e0e6e
MYiLA <polus180@ya.ru>
2020-02-28 08:19:17 +0300

    prototype style completed

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,4 @@
 'use strict';
+// у блока .earnings есть класс .earnings--choose только тогда, когда ни у одного .earning-way нет класса .open
+// при нажатии на кнопку earning-way__tab, родителю earning-way добавить класс open(только после завершения анимации), если его нет (toggleClassList + делегирование со всплытием)
+// c tablet ширины у .earnings__list появляется класс .earnings__list--tab при превращении его в табы(убирает бэк насильственно)
