71206c706318545f3e7c9ab9a558078f9575f101
MYiLA <polus180@ya.ru>
2020-02-29 04:57:18 +0300

    add js logic

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,34 @@
 'use strict';
 // у блока .earnings есть класс .earnings--choose только тогда, когда ни у одного .earning-way нет класса .open
-// при нажатии на кнопку earning-way__tab, родителю earning-way добавить класс open(только после завершения анимации), если его нет (toggleClassList + делегирование со всплытием)
-// c tablet ширины у .earnings__list появляется класс .earnings__list--tab при превращении его в табы(убирает бэк насильственно)
+const earningsElement = document.querySelector('.earnings');
+const earningsListElement = document.querySelector('.earnings__list');
+const earningWayElements = [...earningsElement.querySelectorAll('.earning-way')];
+const earningDescElements = [...earningsElement.querySelectorAll('.earning-way__wrap')];
+const earningMediaElements = [...earningsElement.querySelectorAll('.earning-way__media-wrap')];
+const earningTabElements = [...earningsElement.querySelectorAll('.earning-way__tab')];
+
+const onClickTab = (index) => {
+  earningWayElements.forEach(
+    (element) => {
+      element.classList.remove('open')
+    }
+  );
+
+  if (earningsElement.classList.contains('earnings--choose')) {
+    earningsElement.classList.remove('earnings--choose');
+  };
+
+  if (!earningsListElement.classList.contains('earnings__list--tab')) {
+    earningsElement.classList.add('earnings__list--tab');
+  }
+
+  setTimeout(() => {earningWayElements[index].classList.add('open')}, 1000);
+}
+
+earningTabElements.forEach (
+  (element, index) => {
+    element.addEventListener('click', () => {
+      onClickTab(index)
+    })
+  }
+)
